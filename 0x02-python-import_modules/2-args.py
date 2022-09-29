e772caf6ff253d6e0a8100c2a29e39ee3afe1563
klassikal <alfredcoffie0@gmail.com>
2022-09-29 00:21:20 +0000

    new task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("{} arguments.".format(num_args))
+    else:
+        if num_args == 1:
+            print("{} argument:".format(num_args))
+        else:
+            print("{} arguments:".format(num_args))
+        for idx in range(1, num_args + 1):
+            print("{}: {}".format(idx, sys.argv[idx]))
